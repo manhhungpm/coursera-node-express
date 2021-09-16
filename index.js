011ae2b677d508f6698ba0d1dd1adcfc4fc518ad
manhhungpm <hungnm.nuce60@gmail.com>
2021-09-16 22:50:50 +0700

    Express Route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express'),
     http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,9 +10,11 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public'));
-
 app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
